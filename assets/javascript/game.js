407fa9f6abafdc88be0c59342a859821bac4b3b7
frankcodes <alanforducf@gmail.com>
2020-01-13 18:11:51 -0500

    logic for wins and losses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,54 +1,61 @@
 $(document).ready(function() {
 
-
-    var computerPick = Math.round(Math.random() * 101) + 19;
-    var userNum 
+    var computerPick = Math.floor(Math.random() * 101) + 19;
+    var userNum = 0
     var gameOver = false
     var userWins
     var userLosses
 
+
 $("#random-number").text(computerPick);
-    
+
 // When the player clicks on a crystal, it will add a specific amount of points to the player's total score.
 
-for(var i = 0; i > computerPick; i++)
+if(userNum < computerPick) {
 
 $("#yellow-diamond").on("click", function() {
-    var yellowNum = Math.round(Math.random() * 12) + 1;
+    var yellowNum = Math.floor(Math.random() * 12) + 1;
     userNum = yellowNum + userNum;
+    $("#total-score").text(userNum);
 })
 
 $("#green-diamond").on("click", function() {
-    var greenNum = Math.round(Math.random() * 12) + 1;
+    var greenNum = Math.floor(Math.random() * 12) + 1;
     userNum = greenNum + userNum;
+    $("#total-score").text(userNum);
 })
 
 $("#purple-diamond").on("click", function() {
-    var purpleNum = Math.round(Math.random() * 12) + 1;
+    var purpleNum = Math.floor(Math.random() * 12) + 1;
     userNum = purpleNum + userNum;
+    $("#total-score").text(userNum);
 })
 
 $("#blue-diamond").on("click", function() {
-    var blueNum = Math.round(Math.random() * 12) + 1;
+    var blueNum = Math.floor(Math.random() * 12) + 1;
     userNum = blueNum + userNum;
+    $("#total-score").text(userNum);
 })
 
 // Your game will hide this amount until the player clicks a crystal.
 // When they do click one, update the player's score counter.
 // The player wins if their total score matches the random number from the beginning of the game.
 
-    if (userNum === computerPick) {
+} if (userNum === computerPick) {
         gameOver = true;
         userWins++;
         $("#wins-text").text("Wins: " + userWins);
         $("#game-result").text("You won!!");
+        startGame()
 // The player loses if their score goes above the random number.
 } if (userNum > computerPick) {
         gameOver = true;
         userLosses++;
         $("#losses-text").text("Losses: " + userLosses);
         $("#game-result").text("You lost!!");
-}
+        startGame()
+} 
+
 // The game restarts whenever the player wins or loses.
 
 // When the game begins again, the player should see a new random number. Also, all the crystals will have four new hidden values. Of course, the user's score (and score counter) will reset to zero.
